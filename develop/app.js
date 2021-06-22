9a4487e0a77a68f95d6628f64d5af29f2d1abadf
Chris Beard <cbeard@hnoj.org>
2021-06-22 14:09:20 -0500

    fixing bugs name parameter

--- develop/app.js
+++ develop/app.js
@@ -1,6 +1,7 @@
 const Manager = require("./lib/manager");
 const Engineer = require("./lib/engineer");
 const Intern = require("./lib/intern");
+const Employee = require("./lib/employee");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -8,8 +9,7 @@ const fs = require("fs");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/renderedHTML");
-const Employee = require(".lib/employee");
-const { resolve } = require("path");
+
 const teamArr = []
 
 function employeeQuestions() {
